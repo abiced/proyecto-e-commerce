4790cf42afe602d0c6e6f4fb5048d20baebdadb3
abiced <abicedtrujillo@gmail.com>
2022-08-19 13:12:42 -0300

    Parte 2 terminada

---
+++ js/products.js
@@ -0,0 +1,27 @@
+const infoCars = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+fetch(infoCars)
+.then(response => response.json())
+.then(data => {
+    console.log(data.products)
+    let divCars = document.getElementById('cars');
+    for (let i=0; i < data.products.length; i++) {
+        divCars.innerHTML += `
+        <div onclick="setCatID(${data.products[i].id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${data.products[i].image}" alt="${data.products[i].description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${data.products[i].name} - ${data.products[i].currency} ${data.products[i].cost}</h4>
+                            <small class="text-muted">${data.products[i].soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${data.products[i].description}</p>
+                    </div>
+                </div>
+            </div>
+        `
+}
+}
+)
