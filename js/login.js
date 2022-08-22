d9927a2d86c6f0b375ced766e626410558012bc6
abiced <abicedtrujillo@gmail.com>
2022-08-21 21:59:59 -0300

    Login con google

--- js/login.js
+++ js/login.js
@@ -1,2 +1,15 @@
-let loginForm = document.createElement("text")
-loginForm.innerHTML += "<form "
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+let loginValidation = document.getElementById("loginButton")
+loginValidation.addEventListener( "click", function(event){
+    let emailValidation = document.getElementById("email").value
+    let passwordValidation = document.getElementById("password").value
+    if ( emailValidation.length>0 && passwordValidation.length>0 ){
+        event.preventDefault()
+        window.location.href = "home.html"
+    } else {
+        event.preventDefault()
+        showAlertError()
+    }
+});
